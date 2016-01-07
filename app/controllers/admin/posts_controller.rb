0585eafef946078aa0799e3f01fde04745a6840e
sophia1215 <code.sophiawang@gmail.com>
2016-01-08 03:49:13 +0800

    Searching post

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,8 +1,12 @@
 class Admin::PostsController < Admin::ApplicationController
   def index
     # @posts = Post.all.order(id: :desc).page(params[:page]).per(5)
-    @posts = Post.all.order(id: :desc).page params[:page]
-
+    # byebug
+    if params[:search].present?
+      @posts = Post.where("title LIKE ? OR content LIKE ?", "%#{params[:search]}%", "%#{params[:search]}%").page params[:page]
+    else
+      @posts = Post.all.order(id: :desc).page params[:page]
+    end
   end
 
   def new
