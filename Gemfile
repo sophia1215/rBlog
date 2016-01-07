690315361abf6c5dbd53cc620a62148b6309996f
sophia1215 <code.sophiawang@gmail.com>
2016-01-07 22:56:31 +0800

    Seeding Posts & Tags

--- Gemfile
+++ Gemfile
@@ -40,6 +40,7 @@ gem 'has_scope'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
+  gem 'faker'
 end
 
 group :development do
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,16 @@ moderator = Moderator.create(
   username: "sophia@example.com",
   password: "example"
 )
+
+30.times do
+  post = Post.create(
+    title: Faker::Lorem.sentence(20),
+    content: Faker::Lorem.paragraph,
+    publish: true,
+    moderator: moderator
+  )
+
+  tag = Tag.create(name: Faker::Lorem.word)
+
+  post_tag = PostTag.create(post: post, tag: tag)
+end
