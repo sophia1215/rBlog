502b51775149104ca7d60f6c4a0a77c84ee6866c
sophia1215 <code.sophiawang@gmail.com>
2016-01-09 00:50:19 +0800

    Listing Tags

--- app/helpers/admin/tags_helper.rb
+++ app/helpers/admin/tags_helper.rb
@@ -1,2 +1,15 @@
 module Admin::TagsHelper
+  def create_deletable_button tag
+    if tag.in_use? 
+      link_to '#' do 
+      # <button type="button" class="disabled">Delete</button>
+      content_tag(:button, 'delete', class: 'disabled')
+      end
+    else
+      link_to admin_tag_path(tag),method: :delete, data: {confirm: 'Are you sure?'} do 
+      # <button type="button">Delete</button>
+      content_tag(:button, 'delete')
+      end
+    end 
+  end
 end
