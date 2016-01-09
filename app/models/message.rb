89a953d2d3442f4be3927e83891c8e0850292c8c
sophia1215 <code.sophiawang@gmail.com>
2016-01-09 19:32:51 +0800

    Adding Search & View page

--- app/models/message.rb
+++ app/models/message.rb
@@ -11,4 +11,9 @@
 
 class Message < ActiveRecord::Base
   belongs_to :visitor
+
+  def self.matching_fullname_or_content params
+    joins(:visitor).where("fullname LIKE ? OR content LIKE ?", "%#{params}%", "%#{params}%")
+  end
+
 end
